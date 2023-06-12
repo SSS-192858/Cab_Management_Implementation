605bbdc8c77e1c7160f0caccb24a9e1271a9780c
SankalpKothari0904 <sankalpkothari04@gmail.com>
2023-06-12 22:13:58 +0530

    sql added

---
+++ create_script.sql
@@ -0,0 +1,119 @@
+create database Cab_mangement_system;
+use Cab_mangement_system;
+
+create table user(
+    user_id INTEGER PRIMARY KEY,
+    username varchar(100) NOT NULL,
+    password char(68) NOT NULL
+);
+
+CREATE TABLE role (
+    role_id INTEGER NOT NULL,
+    role_name varchar(100) NOT NULL,
+    constraint pk_role PRIMARY KEY (role_id)
+);
+
+CREATE TABLE user_roles (
+    user_id INTEGER,
+    role_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE driver(
+    driver_id INTEGER,
+    driver_name varchar(100) DEFAULT NULL,
+    driver_email varchar(100) DEFAULT NULL,
+    driver_phno varchar(100) DEFAULT NULL,
+    constraint pk_driver PRIMARY KEY (driver_id)
+);
+
+CREATE TABLE customer(
+    customer_id INTEGER,
+    customer_name varchar(100) DEFAULT NULL,
+    customer_email varchar(100) DEFAULT NULL,
+    customer_phno varchar(100) DEFAULT NULL,
+    constraint pk_customer PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE cab(
+    registration_number varchar(20),
+    model varchar(20) DEFAULT NULL,
+    colour varchar(20) DEFAULT NULL,
+    fare INTEGER,
+    constraint pk_cab PRIMARY KEY (registration_number)
+);
+
+CREATE TABLE request(
+    slno INTEGER,
+    customer_id INTEGER,
+    registration_number varchar(20),
+    start_date timestamp NOT NULL DEFAULT current_timestamp(),
+    end_date datetime DEFAULT NULL,
+    constraint pk_request PRIMARY KEY (slno)
+);
+
+CREATE TABLE cab_driver(
+    slno INTEGER,
+    driver_id INTEGER,
+    registration_number varchar(20),
+    constraint pk_cab_driver PRIMARY KEY (slno)
+);
+
+CREATE TABLE customer_cab(
+    slno INTEGER,
+    customer_id INTEGER,
+    registration_number varchar(20),
+    start_date timestamp NOT NULL DEFAULT current_timestamp(),
+    end_date datetime DEFAULT NULL,
+    constraint pk_cust_cab PRIMARY KEY (slno)
+);
+
+ALTER TABLE user_roles
+ADD CONSTRAINT fk_user_userRoles
+FOREIGN KEY (user_id) REFERENCES user(user_id);
+
+ALTER TABLE user_roles
+ADD CONSTRAINT fk_role_userRoles
+FOREIGN KEY (role_id) REFERENCES role(role_id);
+
+ALTER TABLE customer
+ADD CONSTRAINT fk_customer_user
+FOREIGN KEY (customer_id) REFERENCES user(user_id);
+
+ALTER TABLE driver
+ADD CONSTRAINT fk_driver_user
+FOREIGN KEY (driver_id) REFERENCES user(user_id);
+
+ALTER TABLE request
+ADD CONSTRAINT fk_cab_request
+FOREIGN KEY (registration_number) REFERENCES cab(registration_number);
+
+ALTER TABLE request
+ADD CONSTRAINT fk_cust_request
+FOREIGN KEY (customer_id) REFERENCES customer(customer_id);
+
+ALTER TABLE cab_driver
+ADD CONSTRAINT fk_cab_driver_id
+FOREIGN KEY (driver_id) REFERENCES driver(driver_id);
+
+ALTER TABLE cab_driver
+ADD CONSTRAINT fk_cab_driver_reg_no
+FOREIGN KEY (registration_number) REFERENCES cab(registration_number);
+
+ALTER TABLE customer_cab
+ADD CONSTRAINT fk_cab_customer_reg_no
+FOREIGN KEY (registration_number) REFERENCES cab(registration_number);
+
+ALTER TABLE customer_cab
+ADD CONSTRAINT fk_cust_cab_id
+FOREIGN KEY (customer_id) REFERENCES customer(customer_id);
+
+INSERT INTO role values (1,'ADMIN'),(2,'DRIVER'),(3,'CUSTOMER');
+INSERT INTO user values (1,"admin","$2a$08$qlXB4Pk7sF9ApzimkeQI0eDGvPWGal.Y265Goukid6hdlzz/QN/cy");
+-- test123
+INSERT INTO user values (2,"driver","$2a$08$qlXB4Pk7sF9ApzimkeQI0eDGvPWGal.Y265Goukid6hdlzz/QN/cy");
+INSERT INTO user values (3,"customer","$2a$08$qlXB4Pk7sF9ApzimkeQI0eDGvPWGal.Y265Goukid6hdlzz/QN/cy");
+
+INSERT INTO user_roles (user_id, role_id) SELECT user.user_id, 1 FROM user WHERE user.username = "admin";
+INSERT INTO user_roles (user_id, role_id) SELECT user.user_id, 2 FROM user WHERE user.username = "driver";
+INSERT INTO user_roles (user_id, role_id) SELECT user.user_id, 3 FROM user WHERE user.username = "customer";
